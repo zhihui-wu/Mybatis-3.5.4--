26e0b83a691ee9cef456744c60b52ceea60db292
zhihui_wu01@163.com <1449553037@qq.com>
2019-12-20 09:53:31 +0800

    # 反射工具箱 获取Class的所有getter方法

--- src/main/java/org/apache/ibatis/reflection/Reflector.java
+++ src/main/java/org/apache/ibatis/reflection/Reflector.java
@@ -47,6 +47,8 @@ import org.apache.ibatis.reflection.property.PropertyNamer;
  *
  * 该类中缓存了反射操作需要使用的类的元信息
  *
+ * Reflector是Mybatis反射模块的基础
+ *
  * @author Clinton Begin
  */
 public class Reflector {
@@ -56,25 +58,67 @@ public class Reflector {
    */
   private final Class<?> type;
 
-
+  /**
+   * 可读属性的名称集合，可读属性就是存在相应的getter方法的属性，初始值为空数组
+   */
   private final String[] readablePropertyNames;
+
+  /**
+   * 可写属性的名称集合，可写属性就是存在相应的setter方法的属性，初始值为空数组
+   */
   private final String[] writablePropertyNames;
+
+  /**
+   * 记录了属性相应的setter方法，key是属性名称，value是Invoker对象，它是对setter方法对应Method对象的封装
+   */
   private final Map<String, Invoker> setMethods = new HashMap<>();
+
+  /**
+   * 记录了属性相应的getter方法，key是属性名称，value是Invoker对象，是对getter方法对应Method对象的封装
+   */
   private final Map<String, Invoker> getMethods = new HashMap<>();
+
+  /**
+   * 记录了属性相应的setter方法的参数值类型，key是属性名称，value是setter方法的参数类型
+   */
   private final Map<String, Class<?>> setTypes = new HashMap<>();
+
+  /**
+   * 记录了属性相应的getter方法的返回值类型，key是属性名称，value是setter方法的参数类型
+   */
   private final Map<String, Class<?>> getTypes = new HashMap<>();
+
+  /**
+   * 记录了默认构造方法
+   */
   private Constructor<?> defaultConstructor;
 
+  /**
+   * 记录了所有属性名称的集合
+   */
   private Map<String, String> caseInsensitivePropertyMap = new HashMap<>();
 
+  /**
+   * 在构造方法中解析指定的Class对象，并填充以上集合
+   * @param clazz Class对象
+   */
   public Reflector(Class<?> clazz) {
+    // 初始化type字段
     type = clazz;
+    // 查找clazz的默认构造方法（无参构造方法），具体实现是通过反射遍历所有构造方法
     addDefaultConstructor(clazz);
+    // 处理clazz中的getter方法，填充getMethods集合和getTypes集合
     addGetMethods(clazz);
+    // 处理clazz中的setter方法，填充setMethods集合和setTypes集合
     addSetMethods(clazz);
+    // 处理没有getter/setter方法的字段
     addFields(clazz);
+
+    // 根据getMethods/setMethods集合，初始化可读/写属性的名称集合
     readablePropertyNames = getMethods.keySet().toArray(new String[0]);
     writablePropertyNames = setMethods.keySet().toArray(new String[0]);
+
+    // 初始化caseInsensitivePropertyMap集合，其中记录了所有大写格式的属性名称
     for (String propName : readablePropertyNames) {
       caseInsensitivePropertyMap.put(propName.toUpperCase(Locale.ENGLISH), propName);
     }
@@ -90,10 +134,15 @@ public class Reflector {
   }
 
   private void addGetMethods(Class<?> clazz) {
+    // conflictingGetters集合的key是属性名称，value是对应的getter方法集合，
+    // 因为子类可能覆盖父类的getter方法，所以同一属性名称可能会存在多个getter方法
     Map<String, List<Method>> conflictingGetters = new HashMap<>();
+    // 步骤一：获取当前类以及其父类和接口中定义的所有方法的唯一签名以及相应的Method对象
     Method[] methods = getClassMethods(clazz);
+    // 步骤二：按照JavaBean规范从methods中查找该类中定义的getter方法，将其记录到conflictingGetters集合中
     Arrays.stream(methods).filter(m -> m.getParameterTypes().length == 0 && PropertyNamer.isGetter(m.getName()))
       .forEach(m -> addMethodConflict(conflictingGetters, PropertyNamer.methodToProperty(m.getName()), m));
+    // 对conflictingGetters集合的覆写情况进行处理，并填充getMethods集合和填充getTypes集合
     resolveGetterConflicts(conflictingGetters);
   }
 
@@ -275,33 +324,45 @@ public class Reflector {
    * We use this method, instead of the simpler <code>Class.getMethods()</code>,
    * because we want to look for private methods as well.
    *
+   * 该方法获取指定类以及其父类和接口中定义的所有方法的唯一签名以及相应的Method对象
+   * 用于代替Java反射中的Class.getMethods()方法，因为我们希望把私有方法也获取到。
+   *
    * @param clazz The class
    * @return An array containing all methods in this class
    */
   private Method[] getClassMethods(Class<?> clazz) {
+    // 用于记录指定类中定义的全部方法的唯一签名以及对应的Method对象
     Map<String, Method> uniqueMethods = new HashMap<>();
     Class<?> currentClass = clazz;
     while (currentClass != null && currentClass != Object.class) {
+      // 记录currentClass这个类中定义的全部方法
       addUniqueMethods(uniqueMethods, currentClass.getDeclaredMethods());
 
       // we also need to look for interface methods -
       // because the class may be abstract
+      // 记录接口中定义的方法，因为该类可能是一个抽象类
+      // todo 不理解，是否抽象类使用上述currentClass.getDeclaredMethods()方法无法获取获取到方法，要和接口一起处理
       Class<?>[] interfaces = currentClass.getInterfaces();
       for (Class<?> anInterface : interfaces) {
         addUniqueMethods(uniqueMethods, anInterface.getMethods());
       }
-
+      // 获取父类，继续while循环
       currentClass = currentClass.getSuperclass();
     }
 
     Collection<Method> methods = uniqueMethods.values();
-
+    // 转换成Methods数组返回
     return methods.toArray(new Method[0]);
   }
 
   private void addUniqueMethods(Map<String, Method> uniqueMethods, Method[] methods) {
     for (Method currentMethod : methods) {
+      // 检查方法是不是桥接方法
+      // 桥接方法是为了使Java的泛型方法生成的字节码和1.5版本前的字节码相兼容，由编译器自动生成的方法
+      // 可参考：https://www.cnblogs.com/zsg88/p/7588929.html
       if (!currentMethod.isBridge()) {
+        // 通过getSignature()方法得到的方法签名是：返回值类型#方法名称:参数类型列表。
+        // 例如：Reflector.getSignature(Method)方法的唯一签名是：
         String signature = getSignature(currentMethod);
         // check to see if the method is already known
         // if it is known, then an extended class must have
